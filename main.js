5d79836e93dd7ac82d38459b9258cb5b85f5a40b
dreadderino1987 <sascha_dietrich@outlook.com>
2022-10-02 19:31:55 +0200

    add: debug css

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const navToggle = document.querySelector(".mobile-nav-toogle");
 const primaryNav = document.querySelector(".primary-nav");
+const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
